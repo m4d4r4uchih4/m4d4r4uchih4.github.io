b0d7f9564012c21b7b081e8be5ba86020b006c81
m4d4r4uchih4 <gb358159@gmail.com>
2025-04-04 12:32:04 +0600

    Create blogger.js

---
+++ blogger.js
@@ -0,0 +1,6 @@
+        // Show the popup after 1 second
+        window.addEventListener('load', function() {
+            setTimeout(function() {
+                document.getElementById("cookiePopup").style.display = "flex";
+            }, 1000);
+        });
